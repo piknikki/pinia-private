3d5ee9cba39e30a955236b9e511c21d009a139ee
Nikki Petersen <piknik@me.com>
2022-06-07 12:08:51 -0600

    Add env files and update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from "path";
+// const env = loadEnv(mode, process.cwd(), '')
+//
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  outputDir: import.meta.env.NODE_ENV === 'production' ? 'dist/' : '/',
   server: {
     port: 8000
   }
