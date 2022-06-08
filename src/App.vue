d5014ebd8f1431007ec9fc817117006e9d991ea5
Nikki Petersen <piknik@me.com>
2022-06-08 08:59:17 -0600

    Had to add some stuff back in so I could get it to render right

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link :to="{ name: 'EventList' }">Events</router-link> |
+      <router-link :to="{ name: 'HelloWorld' }">Home (Hello World)</router-link> |
       <router-link :to="{ name: 'About' }">About</router-link>
       |
-      <router-link :to="{ name: 'EventCreate' }">Create Event</router-link>
       <p>Logged in as {{ userStore.firstName }}</p>
     </div>
     <router-view />
